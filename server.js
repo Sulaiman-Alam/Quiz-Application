e69a392033ca8e526ebdd57021757a484682c3f5
Sulaiman Alam <sanewyork1931@gmail.com>
2025-01-15 07:57:16 -0500

    Test change

--- server.js
+++ server.js
@@ -24,23 +24,6 @@ server.on("request", (req, res) => {
             fs.createReadStream(urlObjects[i].filepath).pipe(res);
         }
     }
-    // if (url === "/") { //serve the form html page
-    //     fs.createReadStream("home.html").pipe(res);
-    // } else if (url === "/home.css") {
-    //     fs.createReadStream("home.css").pipe(res);
-    // } else if (url === "/Images/james_holzhauer.jpg") {
-    //     fs.createReadStream("Images/james_holzhauer.jpg").pipe(res);
-    // } else if (url === "/Images/ken_jennings.jpg") {
-    //     fs.createReadStream("Images/ken_jennings.jpg").pipe(res);
-    // } else if (url === "/Images/brad_rutter.jpg") {
-    //     fs.createReadStream("Images/brad_rutter.jpg").pipe(res);
-    // } else {
-    //     for (let i = 0; i < 3; i++) {
-    //         if (url === `/${imageLinks[i]}`) {
-    //             fs.createReadStream(imageLinks[i]).pipe(res);
-    //         }
-    //     }
-    // }
 });
 
 server.listen(port);
